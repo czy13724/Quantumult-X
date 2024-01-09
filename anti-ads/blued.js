73fd6742b727904b806a7916cc2619ca6488a009
Levi <103307567+czy13724@users.noreply.github.com>
2024-01-09 17:43:18 +0800

    Update blued.js

--- anti-ads/blued.js
+++ anti-ads/blued.js
@@ -1,14 +1,17 @@
 # blued男同软件去广告
 # author: Sliverkiss
 
+[rewrite_local]
 //主页推荐直播 
 https://social.blued.cn/users/recommend url reject-dict
 
-//未登录时个人界面广告^https:\/\/social\.blued\.cn\/users\/no_auth\/benefit url reject-dict
+//未登录时个人界面广告
+^https:\/\/social\.blued\.cn\/users\/no_auth\/benefit url reject-dict
 
 //登录后个人界面广告 
 ^https:\/\/social\.blued\.cn\/users\/.+\/more\/ios\?v=2 url script-response-body blued.js 
 
+[mitm]
 hostname=social.blued.cn
 
 //blued脚本内容 
