354eb6f77c12570f5d09b61ec9f7868b9d6826e1
Levi <103307567+czy13724@users.noreply.github.com>
2024-01-28 09:48:39 -0500

    Add files via upload

---
+++ scripts/itranscreen.js
@@ -0,0 +1,86 @@
+/*
+项目名称：itranscreen
+项目作者：LKTEAM23
+下载地址：https://apps.apple.com/us/app/itranscreen-screen-translator/id1663139919
+使用说明：未填写该内容
+使用声明：仅供个人参考学习交流，勿用于其它用途
+
+[rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/itranscreen.js
+
+[mitm]
+hostname = api.revenuecat.com
+*/
+
+let obj =  {
+"request_date_ms": 1704664060864,
+  "request_date": "2024-01-07T21:47:40Z",
+  "subscriber": {
+    "last_seen": "2024-01-07T21:42:02Z",
+    "first_seen": "2024-01-07T21:42:02Z",
+    "original_application_version": "1",
+    "other_purchases": {
+    },
+    "management_url": "https:\/\/t.me\/LKTEAM23",
+    "subscriptions": {
+      "caiyun_10w_1dollar": {
+        "store": "app_store",
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "expires_date": "2099-03-27T07:52:54Z"
+      },
+      "rc_0400_1m": {
+        "store": "app_store",
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "expires_date": "2099-03-27T07:52:54Z"
+      },
+      "rc_0100_1m": {
+        "store": "app_store",
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "expires_date": "2099-03-27T07:52:54Z"
+      },
+      "rc_0200_1m": {
+        "store": "app_store",
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "expires_date": "2099-03-27T07:52:54Z"
+      }
+    },
+    "entitlements": {
+      "pro": {
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "product_identifier": "rc_0200_1m",
+        "expires_date": "2099-03-27T07:52:54Z",
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "store": "app_store"
+      },
+      "pro plus": {
+        "ownership_type": "PURCHASED",
+        "is_sandbox": false,
+        "product_identifier": "rc_0400_1m",
+        "expires_date": "2099-03-27T07:52:54Z",
+        "original_purchase_date": "2020-03-27T07:52:55Z",
+        "purchase_date": "2020-03-27T07:52:54Z",
+        "store": "app_store"
+      }
+    },
+    "original_purchase_date": "2024-01-07T21:38:41Z",
+    "original_app_user_id": "$RCAnonymousID:06923c3f1dfc4f23b1b56c9e24dbdffc",
+    "non_subscriptions": {
+    }
+  }
+}
+
+$done({ body: JSON.stringify(obj), status: 200 });
