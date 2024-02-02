cf1ac70c17bb9b0198525d355ed742f98f0af296
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-02 16:45:03 +0800

    增加免责声明

--- .github/scripts/convert_js_to_plugin.py
+++ .github/scripts/convert_js_to_plugin.py
@@ -64,13 +64,7 @@ def js_to_plugin(js_content):
     # Generate plugin content
     plugin_content = f"""#!name={project_name}
 #!desc={project_desc}
-"""
-# Add the icon url to the plugin content if it exists
-    if img_url:
-        plugin_content += f"#!icon={img_url}\n"
-
-    
-    plugin_content += f"#!====================================
+#!====================================
 #!⚠️【免责声明】
 #!------------------------------------------
 #!1、此脚本仅用于学习研究，不保证其合法性、准确性、有效性，请根据情况自行判断，本人对此不承担任何保证责任。
@@ -79,7 +73,14 @@ def js_to_plugin(js_content):
 #!4、此脚本涉及应用与本人无关，本人对因此引起的任何隐私泄漏或其他后果不承担任何责任。
 #!5、本人对任何脚本引发的问题概不负责，包括但不限于由脚本错误引起的任何损失和损害。
 #!6、如果任何单位或个人认为此脚本可能涉嫌侵犯其权利，应及时通知并提供身份证明，所有权证明，我们将在收到认证文件确认后删除此脚本。
-#!7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。\n\n[MITM]\n{mitm_content_with_append}\n[Script]\n"
+#!7、所有直接或间接使用、查看此脚本的人均应该仔细阅读此声明。本人保留随时更改或补充此声明的权利。一旦您使用或复制了此脚本，即视为您已接受此免责声明。
+"""
+# Add the icon url to the plugin content if it exists
+    if img_url:
+        plugin_content += f"#!icon={img_url}\n"
+
+    
+    plugin_content += f"[MITM]\n{mitm_content_with_append}\n[Script]\n"
 
     # convert and add [task_local] section
     task_local_plugin_content = task_local_to_plugin(js_content)
