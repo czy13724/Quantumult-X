13dffcec8746fdf38c2ecaae573ec3cedcbfbdc3
czy13724 <czy13724@users.noreply.github.com>
2024-02-03 11:21:16 +0000

    已转换为sgmodule文件

--- scripts/FTzhongwenwang.js
+++ scripts/FTzhongwenwang.js
@@ -117,3 +117,5 @@ $done({ body: JSON.stringify(baby) });
 // Adding a dummy plugin change to trigger git commit
 
 // Adding a dummy sgmodule change to trigger git commit
+
+// Adding a dummy sgmodule change to trigger git commit
--- scripts/foodvisor.js
+++ scripts/foodvisor.js
@@ -116,3 +116,5 @@ $done({body: JSON.stringify(Levi)});
 // Adding a dummy plugin change to trigger git commit
 
 // Adding a dummy sgmodule change to trigger git commit
+
+// Adding a dummy sgmodule change to trigger git commit
