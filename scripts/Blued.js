c1630fa256afc774472031172cf940a0a783a0c6
czy13724 <czy13724@users.noreply.github.com>
2024-02-04 11:34:48 +0000

    已转换为stoverride文件

--- scripts/Blued.js
+++ scripts/Blued.js
@@ -112,3 +112,5 @@ $done({ body: JSON.stringify(anye) });
 // Adding a dummy sgmodule change to trigger git commit
 
 // Adding a dummy stoverride change to trigger git commit
+
+// Adding a dummy stoverride change to trigger git commit
--- scripts/foodvisor.js
+++ scripts/foodvisor.js
@@ -130,3 +130,5 @@ $done({body: JSON.stringify(Levi)});
 // Adding a dummy sgmodule change to trigger git commit
 
 // Adding a dummy stoverride change to trigger git commit
+
+// Adding a dummy stoverride change to trigger git commit
