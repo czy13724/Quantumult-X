2748e8d1fb36133c84e0c760aedeaddf9b220867
czy13724 <czy13724@users.noreply.github.com>
2024-02-05 02:15:50 +0000

    已转换为plugin文件

--- scripts/Blued.js
+++ scripts/Blued.js
@@ -115,3 +115,5 @@ $done({ body: JSON.stringify(anye) });
 // Adding a dummy stoverride change to trigger git commit
 
 // Adding a dummy stoverride change to trigger git commit
+
+// Adding a dummy plugin change to trigger git commit
--- scripts/FTzhongwenwang.js
+++ scripts/FTzhongwenwang.js
@@ -134,3 +134,5 @@ $done({ body: JSON.stringify(baby) });
 // Adding a dummy stoverride change to trigger git commit
 
 // Adding a dummy stoverride change to trigger git commit
+
+// Adding a dummy plugin change to trigger git commit
