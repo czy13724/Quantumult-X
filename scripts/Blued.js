a26ab044ce637fb06718d43cf3a248abd284e06a
czy13724 <czy13724@users.noreply.github.com>
2024-02-06 11:39:38 +0000

    已转换为plugin文件

--- scripts/Blued.js
+++ scripts/Blued.js
@@ -127,3 +127,5 @@ $done({ body: JSON.stringify(anye) });
 // Adding a dummy sgmodule change to trigger git commit
 
 // Adding a dummy sgmodule change to trigger git commit
+
+// Adding a dummy plugin change to trigger git commit
--- scripts/chatpdf.js
+++ scripts/chatpdf.js
@@ -160,3 +160,5 @@ $done({body : JSON.stringify(Levi)});
 // Adding a dummy sgmodule change to trigger git commit
 
 // Adding a dummy sgmodule change to trigger git commit
+
+// Adding a dummy plugin change to trigger git commit
