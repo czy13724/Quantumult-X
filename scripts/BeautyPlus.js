fb24300dfc26483422384df3c1784326941eee54
Cody005 <72626458+Cody005@users.noreply.github.com>
2024-02-08 17:07:09 +0100

    Update BeautyPlus.js

--- scripts/BeautyPlus.js
+++ scripts/BeautyPlus.js
@@ -2,7 +2,7 @@
 /*
 项目名称： BeautyPlus
 项目作者： David
-下载地址： https://apps.apple.com/app/id1108042008
+下载地址： https://apps.apple.com/us/app/beautyplus-ai-photo-editor/id622434129?l=en-GB
 使用说明： 先开脚本再打开软件，未成功尝试恢复购买，下载地址请访问script-path链接获取。
 使用声明： 仅供个人参考学习交流，勿用于其它用途
 
