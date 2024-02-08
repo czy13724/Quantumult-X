7bcebbab9ccd558705501eb640b4bce8c6aa58a1
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-08 11:01:29 -0500

    Update BeautyPlus.js

--- scripts/BeautyPlus.js
+++ scripts/BeautyPlus.js
@@ -1,9 +1,10 @@
-/************************
 
-// Quantumult X引用地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/BeautyPlus.js
-// Surge/Shadowrocket 模块地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Surge/BeautyPlus.sgmodule
-// Loon 插件地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Loon/BeautyPlus.plugin
-// Stash 覆写地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Stash/BeautyPlus.stoverride
+/*
+项目名称： BeautyPlus
+项目作者： David
+下载地址： https://apps.apple.com/app/id1108042008
+使用说明： 先开脚本再打开软件，未成功尝试恢复购买，下载地址请访问script-path链接获取。
+使用声明： 仅供个人参考学习交流，勿用于其它用途
 
 [rewrite_local]
 https:\/\/(api\.mr\.pixocial\.com\/v1\/manual_unlock|newbeee-api\.beautyplus\.com\/api\/v1\/asset\/balance) url script-response-body https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/BeautyPlus.js
