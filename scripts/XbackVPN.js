92fc1d800571be309bcace3731034c55dbe8449e
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-10 12:55:40 -0500

    Update XbackVPN.js

--- scripts/XbackVPN.js
+++ scripts/XbackVPN.js
@@ -15,50 +15,51 @@
 
 [rewrite_local]
 
-(https://client-alphant\\.xback\\.io/alphant/api/member/getInfo).* url script-response-body https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/XbackVPN.js
+^https:\/\/client-alphant\.xback\.io\/alphant\/api\/member\/getInfo\?.*$ url script-response-body https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/XbackVPN.js
 
 [mitm]
 hostname = client-alphant.xback.io
 */
 
 
-var objc = JSON.parse($response.body);
+// 获取完整的请求 URL
+let url = $request.url;
 
-var xToken = $request.headers["x-token"];
+// 定义要匹配的URL基础部分（不包含查询参数）
+const baseUrl = 'https://client-alphant.xback.io/alphant/api/member/getInfo';
 
-objc = {
-  "code": "SUCCESS",
-  "success": true,
-  "data": {
-    "expireUnix": 4000103307,
-    "appleSub": "apple_pay",
-    "id": "4",
-    "productNo": "com.xback.subscription.1year",
-    "limited_offer": false,
-    "duration": 366,
-    "type": "yearly",
-    "newToken": xToken,
-    "isEnable": true,
-    "desc": "Yearly",
-    "vipNo": "1",
-    "duration": 99999999,
-    "paypalSub": "",
-    "isPaySinceRegister": true
-  },
-  "msg": "success",
-  "requestId": "2f2bfc10df558190db386c141b24d1a1"
-};
-
-$done({ body: JSON.stringify(objc) });
-
-// Adding a dummy sgmodule change to trigger git commit
-
-// Adding a dummy plugin change to trigger git commit
-
-// Adding a dummy stoverride change to trigger git commit
-
-// Adding a dummy sgmodule change to trigger git commit
-
-// Adding a dummy plugin change to trigger git commit
-
-// Adding a dummy sgmodule change to trigger git commit
+// 如果请求的 URL 正好是基础 URL（确保没有查询参数）
+if (url === baseUrl) {
+    // 解析原始响应体的 JSON 数据
+    let originalBody = JSON.parse($response.body);
+  
+    // 修改响应体中的内容
+    let modifiedBody = {
+        "code": "SUCCESS",
+        "success": true,
+        "data": {
+            "expireUnix": 4000103307,
+            "appleSub": "apple_pay",
+            "id": "4",
+            "productNo": "com.xback.subscription.1year",
+            "limited_offer": false,
+            "duration": 366,
+            "type": "yearly",
+            "newToken": $request.headers["x-token"],
+            "isEnable": true,
+            "desc": "Yearly",
+            "vipNo": "1",
+            "duration": 99999999,
+            "paypalSub": "",
+            "isPaySinceRegister": true
+        },
+        "msg": "success",
+        "requestId": "2f2bfc10df558190db386c141b24d1a1"
+    };
+  
+    // 使用 $done 方法返回修改后的响应体
+    $done({ body: JSON.stringify(modifiedBody) });
+} else {
+    // 如果 URL 包含查询参数，则不修改响应体
+    $done({});
+}
