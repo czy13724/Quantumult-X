504866c37797a74e638ce1f0d05aa02911092d84
Cody005 <72626458+Cody005@users.noreply.github.com>
2024-02-10 14:54:25 +0100

    Update XbackVPN.js

--- scripts/XbackVPN.js
+++ scripts/XbackVPN.js
@@ -26,24 +26,28 @@ hostname = client-alphant.xback.io
 
 */
 
+I modify the code now 
+
 var objc = JSON.parse($response.body);
 
+var xToken = $request.headers["x-token"];
+
 objc = {
- "code": "SUCCESS",
+  "code": "SUCCESS",
   "success": true,
   "data": {
     "expireUnix": 4000103307,
     "appleSub": "apple_pay",
-        "id": "4",
-      "productNo": "com.xback.subscription.1year",
-      "limited_offer": false,
-      "duration": 366,
-      "type": "yearly",
-      "isEnable": true,
-      "desc": "Yearly",
+    "id": "4",
+    "productNo": "com.xback.subscription.1year",
+    "limited_offer": false,
+    "duration": 366,
+    "type": "yearly",
+    "newToken": xToken,
+    "isEnable": true,
+    "desc": "Yearly",
     "vipNo": "1",
-  "duration": 99999999,
-    "newToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDYyNjQ1NDMsImlhdCI6MTcwMzY3MjU0MywiaXNzIjoiWGJhY2sgQ2xpZW50IEFwaSIsIm5iZiI6MTcwMzY3MTU0MywiVVVJRCI6ImVjYzE1MjlmLTI4YmItNGQ3OC05MjE5LTRmMDA1YmRkMmM2MyIsIkdyb3VwSWQiOjIwMDAwMDAsIlVzZXJOYW1lIjoidGV4aXc0MTM0M0B4YWd5bS5jb20iLCJDb21wYW55SWQiOjIwMDAwMDAsIkNvbXBhbnlOYW1lIjoiIiwiQnVmZmVyVGltZSI6ODY0MDAsIkRldmljZUNvZGUiOiJFNTg5MDU0Ri1BRjUyLTRGQjItOURDNy0yNkNEN0JGN0Y4RDIiLCJFbmRVbml4IjoxNzA0MTAzMzA3fQ.9Z9MlgaeOO0c5XH9OkkVsEfUo9dw3Nrdu_tAl4XrUmk",
+    "duration": 99999999,
     "paypalSub": "",
     "isPaySinceRegister": true
   },
@@ -51,7 +55,7 @@ objc = {
   "requestId": "2f2bfc10df558190db386c141b24d1a1"
 };
 
-$done({ body: JSON.stringify(objc)});
+$done({ body: JSON.stringify(objc) });
 // Adding a dummy sgmodule change to trigger git commit
 
 // Adding a dummy plugin change to trigger git commit
