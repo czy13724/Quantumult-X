d4c5ea0ee9df3b7fc7a4373b9ea9788c504d02ec
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-11 16:41:19 +0800

    Update qxXback.js

--- temporary/xbackvpn/qxXback.js
+++ temporary/xbackvpn/qxXback.js
@@ -12,7 +12,7 @@
 项目作者：David&Levi
 下载地址：https://apps.apple.com/app/id1659638467
 使用说明：必须创建一个免费账号，不建议使用appleid注册。
-操作说明：抓包抓取类似于https://client-alphant.xback.io/alphant/api/member/getInfo?deviceCode=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx,找到该地址的响应体(response body),点击json查看(json viewer),将newToken双引号的内容复制出来备用。若你使用boxjs网页则点击我的-数据查看器-输入key为xbackvpn_token并将得到的newToken粘贴进来保存。若使用boxjs管理快捷指令也可以操作。
+操作说明：抓包抓取类似于https://client-alphant.xback.io/alphant/api/member/getInfo?deviceCode=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx,找到该地址的响应体(response body),点击json查看(json viewer),将newToken双引号的内容复制出来备用。若你使用boxjs网页则点击我的-数据查看器-输入key为xbackvpn_token并点击view将得到的newToken粘贴进来保存。若使用boxjs管理快捷指令也可以操作。
 使用声明：⚠️仅供参考，🈲️转载与售卖！
 
 
