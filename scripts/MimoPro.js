b6b6110a7c8c63a43eaa8f1354ba49eeedcbc5d3
czy13724 <czy13724@users.noreply.github.com>
2024-02-12 18:24:21 +0000

    已转换为sgmodule文件

--- scripts/MimoPro.js
+++ scripts/MimoPro.js
@@ -56,3 +56,5 @@ $done({body: JSON.stringify(obj)});
 // Adding a dummy plugin change to trigger git commit
 
 // Adding a dummy stoverride change to trigger git commit
+
+// Adding a dummy sgmodule change to trigger git commit
