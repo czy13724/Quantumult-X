06317ef250489e45833c3501b85f857f3c7f83e2
Levi <103307567+czy13724@users.noreply.github.com>
2024-02-13 09:15:30 +0800

    Update transcribeAI.js

--- scripts/transcribeAI.js
+++ scripts/transcribeAI.js
@@ -1,11 +1,3 @@
-
-
-
-// Quantumult X引用地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/transcribeAI.js
-// Surge/Shadowrocket 模块地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Surge/transcribeAI.sgmodule
-// Loon 插件地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Loon/transcribeAI.plugin
-// Stash 覆写地址： https://raw.githubusercontent.com/czy13724/Quantumult-X/main/Stash/transcribeAI.stoverride
-
 /*
 项目名称： transcribeAI
 项目作者： Levi
@@ -66,21 +58,3 @@ Levi= {
 };
      
 $done({body: JSON.stringify(Levi)});
-
-// Adding a dummy sgmodule change to trigger git commit
-
-// Adding a dummy plugin change to trigger git commit
-
-// Adding a dummy stoverride change to trigger git commit
-
-// Adding a dummy sgmodule change to trigger git commit
-
-// Adding a dummy plugin change to trigger git commit
-
-// Adding a dummy stoverride change to trigger git commit
-
-// Adding a dummy sgmodule change to trigger git commit
-
-// Adding a dummy plugin change to trigger git commit
-
-// Adding a dummy stoverride change to trigger git commit
