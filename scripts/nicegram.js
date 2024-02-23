58213902cc8ac1f4eaaef0cc321eab28888202a9
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-02-24 00:31:57 +0800

    更新 nicegram.js

---
+++ scripts/nicegram.js
@@ -0,0 +1,21 @@
+/*
+项目名称： nicegram
+项目作者： David
+下载地址： https://apps.apple.com/us/app/nicegram-ai-chat-for-telegram/id1608870673?l=en-GB
+使用说明： 先开脚本再打开软件，未成功尝试恢复购买，下载地址请访问script-path链接获取。
+使用声明： 仅供个人参考学习交流，勿用于其它用途
+
+[rewrite_local]
+https://nicegram.cloud/api/v6/telegram/auth|user|info url script-response-body https://raw.githubusercontent.com/czy13724/Quantumult-X/main/scripts/nicegram.js
+
+[mitm]
+hostname = nicegram.cloud
+*/
+var body = $response.body;
+
+body = body.replace(/"store_subscription":false/g, '"store_subscription":true');
+body = body.replace(/"lifetime_subscription":false/g,'"lifetime_subscription":true');
+body = body.replace(/"subscription":false/g,'"subscription":true');
+
+
+$done({ body });
