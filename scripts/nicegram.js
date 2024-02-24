2f055d0092e82a1188fa764b8bc38e10ef366d39
anyehttp <93598563+anyehttp@users.noreply.github.com>
2024-02-24 15:05:58 +0800

    更新 nicegram.js

--- scripts/nicegram.js
+++ scripts/nicegram.js
@@ -7,7 +7,7 @@
 项目名称： nicegram
 项目作者： David
 下载地址： https://apps.apple.com/us/app/nicegram-ai-chat-for-telegram/id1608870673?l=en-GB
-使用说明： 先开脚本再打开软件，未成功尝试恢复购买，下载地址请访问script-path链接获取。
+使用说明： 要激活高级版，请单击底部的“助手”，然后单击“释放您的 Nicegram 福利”，最后单击底部的“开始”。完成此操作后，Premium 应该已解锁
 使用声明： 仅供个人参考学习交流，勿用于其它用途
 
 [rewrite_local]
